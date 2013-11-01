4f3b65f7eb98b939f84c7c77a950db90f3cf8e7e
Tobias Savinainen <tobias@savinainen.se>
2013-11-02 00:34:24 +0100

    Change to a JFrame instead

--- SQL Client/src/SQLClient/SQLClientExecutionResult.java
+++ SQL Client/src/SQLClient/SQLClientExecutionResult.java
@@ -6,35 +6,43 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
+import javax.swing.JTable;
+import javax.swing.border.Border;
 import javax.swing.border.EmptyBorder;
+import javax.swing.border.EtchedBorder;
 import javax.swing.border.TitledBorder;
 
-public class SQLClientExecutionResult extends JPanel {
+public class SQLClientExecutionResult extends JFrame {
     
-    public SQLClientExecutionResult() {
-        setLayout(new BorderLayout());
+    public SQLClientExecutionResult(JTable table) {
+        setLayout(new BorderLayout());      
         
-        JPanel panel1 = new JPanel();
-        panel1.add(new JScrollPane(new JTextArea(20, 50)));
-        panel1.setBorder(BorderFactory.createCompoundBorder(new EmptyBorder(10, 10, 0, 10), new TitledBorder("SQL Execution Results")));
+        Border border1 = BorderFactory.createCompoundBorder(new EmptyBorder(25, 25,  5, 25), new TitledBorder("SQL Execution Results"));
+        Border border2 = BorderFactory.createCompoundBorder(new EmptyBorder(10, 10, 10, 10), new EtchedBorder());
         
-        JButton button = new JButton("Clear");
-        button.addActionListener(new ButtonClearListener());
+        JPanel panel1 = new JPanel(new BorderLayout());
+        panel1.add(table.getTableHeader(), BorderLayout.NORTH);
+        panel1.add(table, BorderLayout.CENTER);
+        panel1.setBorder(BorderFactory.createCompoundBorder(border1, border2));
         
-        JPanel panel2 = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+        JButton button = new JButton("Close");
+        button.addActionListener(new ButtonCloseListener());
+        
+        JPanel panel2 = new JPanel(new FlowLayout(FlowLayout.CENTER));
         panel2.add(button);
         
         add(panel1, BorderLayout.NORTH);
         add(panel2, BorderLayout.CENTER);
+        
+        pack();
     }
-    
-    private class ButtonClearListener implements ActionListener {
+      
+    private class ButtonCloseListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
-            
+            dispose();
         }
     }
 }
